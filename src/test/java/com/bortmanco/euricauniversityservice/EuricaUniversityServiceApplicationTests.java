6f7fd64f54224816a6458c27b89a0b78d1788dc8
Mikhail Butorin <mikhail.butorin@axenix.pro>
2024-03-27 18:03:25 +0500

    Initial commit

---
+++ src/test/java/com/bortmanco/euricauniversityservice/EuricaUniversityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bortmanco.euricauniversityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuricaUniversityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
